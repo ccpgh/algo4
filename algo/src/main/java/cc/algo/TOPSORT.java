6f6ba0191882baf6c0a67b738f9d40978fac3baf
cc <claude.cockfield@bison.howard.edu>
2024-10-16 04:03:00 -0400

    backup

--- algo/src/main/java/cc/algo/TOPSORT.java
+++ algo/src/main/java/cc/algo/TOPSORT.java
@@ -6,33 +6,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 
 public class TOPSORT {
-
-  public class Vertex {
-
-    public enum COLOR { COLOR_WHITE, COLOR_BLACK };
-    
-    final private Character _name;
-
-    private Vertex _PI = null;
-    
-    private COLOR _color;
-
-    private Integer _start_time = 0;
-    
-    private Integer _end_time = 0;
-    
-    public Vertex(char name, COLOR color) {
-      
-      _name = name;
-      
-      _color = color;
-    }
-    
-    public Character name() {
-      
-      return _name;
-    }
-  }
   
   final private LinkedHashMap<Vertex, List<Vertex>> _adjacents = new LinkedHashMap<>();
 
@@ -82,9 +55,10 @@ public class TOPSORT {
     StringBuilder buffer = new StringBuilder();
     
     buffer.append("{");
+    buffer.append("\n");
     
     _adjacents.keySet().forEach(key->{
-      buffer.append(key.name());
+      buffer.append(key.toString());
       buffer.append(" -> ");
       _adjacents.get(key).forEach(v->{
         buffer.append(v.name());
@@ -104,7 +78,7 @@ public class TOPSORT {
         
     Arrays.asList(_ts).forEach(line->{
       Vertex vertex = new Vertex(line.charAt(0), 
-          Vertex.COLOR.COLOR_WHITE);
+          Vertex.COLOR.WHITE);
       _adjacents.put(vertex, new ArrayList<>());
       mapper.put(vertex.name(), vertex);
     });
@@ -116,99 +90,5 @@ public class TOPSORT {
         });
       }
     });
-  }
-
-  /*
-  public int M() {
-    
-    return _M;
-  }
-  
-  public int n() {
-    
-    return _n;
-  }
-
-  public String V() {
-    
-    return Common.fold(_V);
-  }
-  
-  public String W() {
-    
-    return Common.fold(_W);
-  }
-  
-  public boolean solve() {
-
-    int[] optimal = valuation(0, _n);
-
-    _solution = "(value, weight, count) = (" + optimal[0] + "," + optimal[1] + "," + optimal[2] + ")";
-    
-    return optimal[0] != Integer.MIN_VALUE && optimal[1] != Integer.MAX_VALUE;
-  }
-
-  public int[] valuation(int i, int j) {
-
-    String key = "" + i + "-" + j;
-       
-    if (_memory.containsKey(key)) {
-      
-      return _memory.get(key);
-    }
-
-    if (i == j) {
-    
-      int[] value = new int[] { 0, 0, 0 };
-      
-      if (value[1] > _M) {
-      
-        value = _default;       
-      } 
-            
-      _memory.put(key, value);
-      
-      return value;
-    }
-
-    if ((j-i) == 1) {
-            
-      int[] value = new int[] { _V[i], _W[i], 1 };
-
-      if (value[1] > _M) {
-        
-        value = _default;       
-      } 
-
-      _memory.put(key, value);
-
-      return value;
-    }
-
-    int[] result = _default;
-    
-    for (int k = i; k < j; k++) {
-
-      int[] lhs = valuation(i, k);
-      
-      int[] rhs = valuation(k + 1, j);
-
-      _memory.put("" + i + "-" + k, lhs);
-
-      _memory.put("" + (k+1) + "-" + j, rhs);
-
-      if (lhs[1] <= _M && rhs[1] <= _M) {
-
-        if ((lhs[1] + rhs[1]) <= _M &&
-            (lhs[0] + rhs[0]) > result[0]) {
-
-          result = new int[] { lhs[0] + rhs[0], lhs[1] + rhs[1], lhs[2] + rhs[2]};
-        }
-      } 
-    }
-    
-    return result;
-  }
-      */
-  
+  }  
 }
---
+++ algo/src/main/java/cc/algo/Vertex.java
@@ -0,0 +1,80 @@
+package cc.algo;
+
+public class Vertex {
+
+  public enum COLOR { WHITE, BLACK };
+  
+  final private Character _name;
+
+  private Vertex _PI = null;
+  
+  private COLOR _color;
+
+  private Integer _start_time = 0;
+  
+  private Integer _end_time = 0;
+  
+  public Vertex(char name, COLOR color) {
+    
+    _name = name;
+    
+    _color = color;
+  }
+  
+  public Character name() {
+    
+    return _name;
+  }
+  
+  public COLOR color() {
+    
+    return _color;
+  }
+
+  public int start_time() {
+    
+    return _start_time;
+  }
+
+  public void start_time(int time) {
+    
+    _start_time = time;
+  }
+
+  public int end_time() {
+    
+    return _end_time;
+  }
+  
+  public void end_time(int time) {
+    
+    _end_time = time;
+  }
+
+  public void pi(Vertex PI) {
+    
+    _PI = PI;
+  }
+  
+  public Vertex pi() {
+    
+    return _PI;
+  }
+
+  public String toString() {
+    
+    StringBuilder buffer = new StringBuilder();
+    
+    buffer.append("{");
+    buffer.append(_name);
+    buffer.append(" color: ");
+    buffer.append(_color);
+    if (_PI != null) {
+      buffer.append(" parent: ");
+      buffer.append(_PI.name());
+    }
+    buffer.append("}");    
+    
+    return buffer.toString();
+  }
+}
